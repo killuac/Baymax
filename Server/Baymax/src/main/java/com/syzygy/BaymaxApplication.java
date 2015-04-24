b5fd2ffa9b29cf58883f51332fdd6f829bf1cab1
Killua <killua.liu@gmail.com>
2015-04-24 19:07:23 +0800

    Init project with IntelliJ IDEA

---
+++ Server/Baymax/src/main/java/com/syzygy/BaymaxApplication.java
@@ -0,0 +1,12 @@
+package com.syzygy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaymaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaymaxApplication.class, args);
+    }
+}
